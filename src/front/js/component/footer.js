6724beb4c2d335f693537a91b6c57b183c572b0a
Aily Gucfa <126049721+AilyGucfa@users.noreply.github.com>
2023-09-19 05:14:02 +0000

    Feature-2/Footer-Creation fixed the css for the footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -4,19 +4,13 @@ import { Link } from "react-router-dom";
 export const Footer = () => (
   <footer className="footer mt-auto py-3 text-center">
     <div className="container">
-      <div className="row">
-        <div className="col">
-          <Link to="/contact" className="footerLinks">Contact</Link>
-        </div>
-        <div className="col">
-          <Link to="/about" className="footerLinks">About</Link>
-        </div>
-        <div className="col">
-          <Link to="/follow-us" className="footerLinks">Follow Us</Link>
-        </div>
-      </div>
-      <div className="col">
-		<br></br>
+    <div className="footerLinks">
+					<Link to="/contact">Contact</Link>
+					<Link to="/aboutUs">About</Link>
+					<Link to="/signup">Follow Us</Link>
+		</div>
+    <div>
+		  <br />
         &copy; 2023 Helping Hands. All rights reserved.
       </div>
     </div>
