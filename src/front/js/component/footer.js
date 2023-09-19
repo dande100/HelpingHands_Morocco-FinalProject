40a628cf4b14a826d2afd78830421d3edc80721c
Aily Gucfa <126049721+AilyGucfa@users.noreply.github.com>
2023-09-19 02:01:56 +0000

    feature-2/footer-creation , created a footer but need some css

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
+  <footer className="footer mt-auto py-3 text-center">
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <Link to="/contact" className="footerLinks">Contact</Link>
+        </div>
+        <div className="col">
+          <Link to="/about" className="footerLinks">About</Link>
+        </div>
+        <div className="col">
+          <Link to="/follow-us" className="footerLinks">Follow Us</Link>
+        </div>
+      </div>
+      <div className="col">
+		<br></br>
+        &copy; 2023 Helping Hands. All rights reserved.
+      </div>
+    </div>
+  </footer>
 );
