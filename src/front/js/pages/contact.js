010458449b9b8fb5bb9d46b62fa5d5aed89860fa
Jessmor2 <135068935+Jessmor2@users.noreply.github.com>
2023-09-20 23:15:07 +0000

    Updated with lastest pull from main

--- src/front/js/pages/contact.js
+++ src/front/js/pages/contact.js
@@ -14,20 +14,26 @@ export const Contact = () => {
                 </h1>
             </div>
             <div className="container mt-4" id="contactPageP">
-                <p>
-                If you have any questions for our team send us a message below. If you are interested in ways you can help or 
-                    about volunteering in Morocco please fill out the form below and we will reach out to you about our next 
-                    volunteer trip schedules and other ways to get involved!
-                </p>
+                <div class="row">
+                    <div className="col-2"></div>
+                    <div class="col-8">
+                        <p>
+                            If you have any questions for our team send us a message below. If you are interested in ways you can help or 
+                                about volunteering in Morocco please fill out the form below and we will reach out to you about our next 
+                                volunteer trip schedules and other ways to get involved!
+                        </p>
+                    </div>
+                    <div className="col-2"></div>
+                </div>
             </div>
             <div class="container mt-4">
                 <div class="row">
-                    <div class="col-5">
+                    <div class="col-6">
                         <img className="aftermathPhoto1" src="https://s.france24.com/media/display/e57eef1a-4eee-11ee-b700-005056bf30b7/w:1280/p:1x1/2023-09-09T081028Z_1417888762_RC2U43AEAKXE_RTRMADP_3_MOROCCO-QUAKE.JPG"></img>
                         <br></br><br></br>
                         <img className="aftermathPhoto2" src="https://marvel-b1-cdn.bc0a.com/f00000000209887/www.projecthope.org/wp-content/uploads/2023/09/b17c7677-8481-4083-a711-1ac421de8ef6.jpg"></img>
                     </div>
-                    <div class="col-7">
+                    <div class="col-6">
                         <div class="mb-3">
                             <label for="Name" className="form-label">First & Last Name</label>
                             <input type="text" class="form-control" id="formNameInput" placeholder="John Doe"></input>
@@ -44,7 +50,7 @@ export const Contact = () => {
                             <label for="Comments" className="form-label">Questions/Comments</label>
                             <textarea type="text-area" class="form-control" id="formCommentsInput" rows="5" placeholder="Let us know how we can help you help others."></textarea>
                         </div>
-                        <button className="contactFormSubmit">Submit</button>
+                        <button className="btn btn-primary btn-lg" id="contactFormSubmit">Submit</button>
                     </div>
                     
                 </div>
