17dd6d2a0591637715f899b6e256aee5d95a8e6b
Aily Gucfa <126049721+AilyGucfa@users.noreply.github.com>
2023-09-20 18:29:48 +0000

    feature-3/homepage-creation added donate button

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import sliderBGImageUrl from "../../img/slider_bg.jpg";
 import "../../styles/home.css";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
@@ -13,6 +14,9 @@ export const Home = () => {
 				<h1>Give A Hand To Make <br /> The <span id="better">Better</span> World</h1>
 				<p>Following the recent earthquake in Morocco, your donation can truly make a difference. Join us in helping the affected communities recover and rebuild. Your contribution offers hope and strength to those in need, and together, we can work towards a resilient Morocco.</p>
 			</div>
+			<Link to="/signup" >
+				<btn className="donate-button">Donate Now</btn>
+			</Link>
 		</div>
 	);
 };
