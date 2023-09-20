996d9875bb4dee71a931ab7f272ee7052e43fcf1
Aily Gucfa <126049721+AilyGucfa@users.noreply.github.com>
2023-09-20 18:52:27 +0000

    feature-3/homepage-creation added css for the donate button and created a background color for the progressbar section

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -12,11 +12,16 @@ export const Home = () => {
 			<img className="backgroundImage" src={sliderBGImageUrl} alt="Slider Background" style={{ maxWidth: "100%", height: "auto" }} />
 			<div className="text-overlay">
 				<h1>Give A Hand To Make <br /> The <span id="better">Better</span> World</h1>
-				<p>Following the recent earthquake in Morocco, your donation can truly make a difference. Join us in helping the affected communities recover and rebuild. Your contribution offers hope and strength to those in need, and together, we can work towards a resilient Morocco.</p>
+				<p>Following the recent earthquake in Morocco, your donation can truly make a difference. <br /> Join us in helping the affected communities recover and rebuild. <br /> Your contribution offers hope and strength to those in need, and together, <br /> we can work towards a resilient Morocco.</p>
 			</div>
 			<Link to="/signup" >
 				<btn className="donate-button">Donate Now</btn>
 			</Link>
+
+			<div className="progress-bar">
+
+			</div>
+
 		</div>
 	);
 };
