f9eea317bcb90e613fc06ca8ce72ce184143f786
Aily Gucfa <126049721+AilyGucfa@users.noreply.github.com>
2023-09-20 23:42:18 +0000

    feature-3/homepage-creation deleted navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,23 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logoImageUrl from "../../img/logo.png";
 
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
 				<Link to="/">
-					 <img 
-					 	className="logo" 
-						src={logoImageUrl}
-						style={{ width: "220px", height: "220px" }}/>
+					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
 				</Link>
-				<div className="navLinks">
-					<Link to="/home">Home</Link>
-					<Link to="/contact">Contact</Link>
-					<Link to="/aboutUs">About Us</Link>
-					<Link to="/login" className="button-link login-button">Login</Link>
-          			<Link to="/signup" className="button-link signup-button">Sign Up</Link>
+				<div className="ml-auto">
+					<Link to="/demo">
+						<button className="btn btn-primary">Check the Context in action</button>
+					</Link>
 				</div>
 			</div>
 		</nav>
