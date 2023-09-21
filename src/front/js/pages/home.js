06fed09724fe01ef6d050c2aa504f3aba3ded4db
Aily Gucfa <126049721+AilyGucfa@users.noreply.github.com>
2023-09-21 04:59:03 +0000

    feature-3/homepage-creation progress bar added and css added

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -18,37 +18,40 @@ export const Home = () => {
 			<div className="text-overlay">
 				<h1>Give A Hand To Make <br /> The <span id="better">Better</span> World</h1>
 				<p>Following the recent earthquake in Morocco, your donation can truly make a difference. <br /> Join us in helping the affected communities recover and rebuild. <br /> Your contribution offers hope and strength to those in need, and together, <br /> we can work towards a resilient Morocco.</p>
+				<Link to="/signup">
+					<button className="donate-button">Donate Now</button>
+				</Link>
 			</div>
-			<Link to="/signup">
-				<button className="donate-button">Donate Now</button> 
-			</Link>
+
 			<div className="progress-bar">
-				<div className="row">
-					<div className="col-md-3 col-sm-6">
-						<div className="progress blue">
-							<span className="progress-left">
-								<span className="progress-bar"></span>
-							</span>
-							<span className="progress-right">
-								<span className="progress-bar"></span>
-							</span>
-							<div className="progress-value">90%</div>
+				<div className="body">
+					<div className="row">
+						<div className="col-md-3 col-sm-6">
+							<div className="progress blue">
+								<span className="progress-left">
+									<span className="progress-bar"></span>
+								</span>
+								<span className="progress-right">
+									<span className="progress-bar"></span>
+								</span>
+								<div className="progress-value">90%</div>
+							</div>
 						</div>
+						<div className="col-md-3 col-sm-6l progress-bar-statement">
+							<h2>Urgent Relief</h2>
+							<p>In the wake of a devastating earthquake that has left communities in Morocco reeling,<br /> we are calling upon compassionate individuals, organizations, and communities worldwide  <br /> to come together and join our mission to be a beacon of hope in aiding Morocco's earthquake recovery efforts.</p>
+							<Link to="/signup">
+								<button className="donate-button-statement">Donate Now</button>
+							</Link>
+						</div>
+					</div>
+
+					<div className="row row-cols-auto raised-goal">
+						<div className="col ms-5 ps-1 ">Raised<br />$37,500</div>
+						<div classNames="col ms-3 ps-1 ">Goal<br />$50,000</div>
 					</div>
 				</div>
-				<div className="row">
-					<div className="col-md-3 col-sm-6">
-						<div className="raised-goal-container">
-							<p className="raised">Raised <br /> $35,000</p>
-						</div>
-					</div>
-					<div className="col-md-3 col-sm-6">
-						<div className="raised-goal-container">
-							<p className="goal">Goal: <br />35,000</p>
-						</div>
-					</div>
-				</div>
-			</div>
+			</div >
 		</div>
 	);
 };
