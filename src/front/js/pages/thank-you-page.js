0e6712b840c9277522b57ed436a0538fb2cd2220
Vasanthi Dande <135659465+dande100@users.noreply.github.com>
2023-10-18 19:23:59 -0500

    Update thank-you-page.js

--- src/front/js/pages/thank-you-page.js
+++ src/front/js/pages/thank-you-page.js
@@ -1,6 +1,5 @@
 
 import React, { useContext, useState, useEffect } from "react";
-import React, { useState, useContext, useEffect } from "react";
 
 import { Context } from "../store/appContext";
 import CircularProgressBar from "./circularProgressBar";
