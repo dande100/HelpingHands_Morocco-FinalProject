e38b846c498835e42b22f4cce69b7c4a0ac3ed75
Vasanthi Dande <135659465+dande100@users.noreply.github.com>
2023-10-20 15:21:07 +0000

    deploying to heroku

--- migrations/versions/934efb4bf77b_.py
+++ migrations/versions/934efb4bf77b_.py
@@ -1,6 +1,6 @@
 """empty message
 
-Revision ID: d11de0cd7570
+Revision ID: 934efb4bf77b
 Revises: 
 Create Date: 2023-10-20 15:14:32.410779
 
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'd11de0cd7570'
+revision = '934efb4bf77b'
 down_revision = None
 branch_labels = None
 depends_on = None
